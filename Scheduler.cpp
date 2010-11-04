fb62027b6443f3ca9b7ad9557051810d902f4579
Karl <karl@cancelliere.ca>
2010-11-04 15:35:28 -0400

    Added constructor and release_processor functions

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -1,26 +1,71 @@
 #include "Scheduler.h"
-/*
-int PQ::release_processor( ) { return -2; }  
-int PQ::change_priority( PCB * target, new_priority ) { return -2; }   
-int PQ::process_switch( ) {return -2;}
 
-int PQ::add_ready_process( PCB * target ) { return -2;}
-int PQ::block_process (PCB * target, string reason ) {return -2; }
-int PQ::unblock_process( PCB * target ) {return -2; }
+/*
+Constructor
+
+arguments: 
+	currentProcess will be the first process to execute
+	readyProcs[]: An array of PCB pointers that point to all the procs that will start on the ready queue
+	
+*/
+
+Scheduler::Scheduler(PCB* currentProcess, PCB* readyProcs[]) {
+	_currentProcess = currentProcess;
+	
+	//Add all readyProcs to the ready queue.
+	int sizeOfReadyProcs = sizeof( readyProcs ) / sizeof( readyProcs[0] );
+	
+	for(int i=0; i<sizeOfReadyProcs; i++) {
+		_readyProcs.penqueue( readyProcs[i] );
+	}
+
+}
+
+/*
+Yields the CPU to the next available process, if there is one waiting.
+*/
+void Scheduler::release_processor( ) { 
+	//Save the context of the currently executing proc.
+	//Does some crazy context save shinanigans need to be done here???
+	_currentProcess->context->save();
+
+	//Put currentProcess on the ready queue.
+	_readyProcs.penqueue( _currentProcess );
+	
+	//Allow next process to start executing.
+	//Note that if there is nothing waiting,
+	//Then the single existing proc will be
+	//put back on the CPU. Therefore, this
+	//edgecase is covered.
+	_currentProcess = _readyProcs.pdequeue();
+	
+	//Restore this proc's context
+	_currentProcess.context.save();
+} 
+
+
+/*
+ 
+int Scheduler::change_priority( PCB * target, new_priority ) { return -2; }   
+int Scheduler::process_switch( ) {return -2;}
+
+int Scheduler::add_ready_process( PCB * target ) { return -2;}
+int Scheduler::block_process (PCB * target, string reason ) {return -2; }
+int Scheduler::unblock_process( PCB * target ) {return -2; }
 */
 //Returns if a process is currently blocked on envelope
 /*
-int PQ::is_blocked_on_envelope( PCB * target ) {return -2; }
+int Scheduler::is_blocked_on_envelope( PCB * target ) {return -2; }
 */
 /*
-PCB * PQ::current_process {return NULL;} // Executing state
+PCB * Scheduler::current_process {return NULL;} // Executing state
 
-PQ * ready_procs() {return NULL;} // Ready to execute state
-PQ * blocked_env_procs() {return NULL;} // Blocked on resource state
-LL_List blocked_msg_recieve() {return NULL;}
+PQ * Scheduler::ready_procs() {return NULL;} // Ready to execute state
+PQ * Scheduler::blocked_env_procs() {return NULL;} // Blocked on resource state
+List Scheduler::blocked_msg_recieve() {return NULL;}
 */
 /*
-int context_switch( PCB * next_proc ) {return -2;}
+int Scheduler::context_switch( PCB * next_proc ) {return -2;}
 */
 int context_save( ) { return -2; }
 
--- Scheduler.h
+++ Scheduler.h
@@ -8,10 +8,10 @@ Dependencies are not yet complete.
 
 class Scheduler {
     public:
-/*
-			Scheduler::Scheduler(); //Constructor!
-*/			
-			int release_processor( );   
+
+			Scheduler(PCB* currentProcess, PCB* readyProcs[]); //Constructor!
+			
+			void release_processor( );   
 /*			int change_priority( PCB * target, new_priority );   */
 			int process_switch( );
        
@@ -34,10 +34,10 @@ class Scheduler {
 
 			//Members
 /*			    
-      PCB * current_process; // Executing state
+      PCB * _currentProcess; // Executing state
 
-			PQ ready_procs; // Ready to execute state
-      PQ blocked_env_procs; // Blocked on resource state
-      LL_List blocked_msg_recieve;
+			PQ _readyProcs; // Ready to execute state
+      PQ _blockedEnvProcs; // Blocked on resource state
+      Queue _blockedMsgRecieve;
 */      
 };
