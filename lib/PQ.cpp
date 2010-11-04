cf46dbe8bf3a81c033b929bd1bf7a70469b51dee
Karl <karl@cancelliere.ca>
2010-11-04 19:36:16 -0400

    First pass on PQ class complete

--- lib/PQ.cpp
+++ lib/PQ.cpp
@@ -8,11 +8,40 @@ using namespace std;
 		nPriority: the number of priority levels that will be used.
 */
 PQ::PQ(int nPriority) {
-	Queue master[nPriority]
+	Queue _master[nPriority];
 };
 
+/*
+Enqueues newData into the queue with priorityLevel priority.
+*/
 void PQ::penqueue (PCB newData, priorityLevel)
 {
-	master[ priorityLevel ];
+	//Check to make sure that the priority level exists
+	masterLength = sizeof( _master ) / sizeof( _master[0] );
+	if (priorityLevel >= masterLength) {
+		//Then the priorityLevel is too high, so return error
+		return 1;
+	}
+	
+	//Enqueue the data at the requested priority level.	
+	return _master[ priorityLevel ].enqueue( newData );
+	
 }
-PCB * PQ::pdequeue () {return -2;}       
+
+/*
+Dequeues a pointer to an itemType. Returns null if there is nothing to dequeue.
+*/
+itemType * PQ::pdequeue () 
+{
+	masterLength = sizeof( master ) / sizeof( master[0] );
+	itemType* returnVal;
+	for( int i; i < masterLength; i++) {
+		//Try dequeuing
+		if (returnVal = _master[i].dequeue) {
+			return returnVal;
+		}	
+	}
+	
+	return NULL; //The dequeue has failed, nothing to dequeue.	
+}
+
