df5cad69ae5b96f4d9a7861321f8343dcaec2245
Karl <karl@cancelliere.ca>
2010-11-06 18:34:05 -0400

    Finished Scheduler class - first pass

--- Scheduler.h
+++ Scheduler.h
@@ -17,27 +17,21 @@ class Scheduler {
        
 
 			//Place new process on ready queue
-/*
       bool add_ready_process( PCB * target );
 			bool block_process (PCB * target, string reason );
 			bool unblock_process( PCB * target );
-*/
+			
 			//Returns if a process is currently blocked on envelope
-/*
-			bool is_blocked_on_envelope( PCB * target );
-*/
+			bool is_blocked( PCB * target );
+
     private:
-/* 
       int context_switch( PCB * next_proc );
-*/
       int context_save( );
 
 			//Members
-/*			    
       PCB * _currentProcess; // Executing state
 
 			PQ _readyProcs; // Ready to execute state
       Queue _blockedEnv; // Blocked on resource state
       Queue _blockedMsgRecieve;
-*/      
 };
